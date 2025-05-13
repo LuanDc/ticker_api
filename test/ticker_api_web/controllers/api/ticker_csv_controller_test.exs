25b49e9256bf0e41bec986c1ea8583526c1f22f3
luan fellipe <luan.fellipe@live.com>
2025-05-13 18:38:12 -0300

    test: uploads ticker csv file to the ticker csv bucket

--- test/ticker_api_web/controllers/api/ticker_csv_controller_test.exs
+++ test/ticker_api_web/controllers/api/ticker_csv_controller_test.exs
@@ -8,14 +8,14 @@ defmodule TickerApi.TickerCsvControllerTest do
   test "uploads ticker csv file to the ticker csv bucket", %{conn: conn} do
     file =
       %Plug.Upload{
-        content_type: "text/csv",
-        filename: "29-04-2025_NEGOCIOSAVISTA.txt",
-        path: "test/support/fixtures/29-04-2025_NEGOCIOSAVISTA.txt"
+        content_type: "application/zip",
+        filename: "29-04-2025_NEGOCIOSAVISTA.txt.zip",
+        path: "test/support/fixture/29-04-2025_NEGOCIOSAVISTA.txt.zip"
       }
 
-    expect(B3TickersBucketMock, :upload!, fn path ->
+    expect(B3TickersBucketMock, :upload!, fn path, _image_binary ->
       assert path == file.path
-      :done
+      %{body: "", headers: [{"Content-Length", "0"}], status_code: 200}
     end)
 
     assert conn
