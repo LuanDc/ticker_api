ed1aa65da01f9eb89df9e43434984b288c9e6654
luan fellipe <luan.fellipe@live.com>
2025-06-01 15:35:31 -0300

    build: creates sns and sqs resources by terraform

--- terraform.tf
+++ terraform.tf
@@ -10,9 +10,88 @@ provider "aws" {
 
   endpoints {
     s3             = "http://s3.localhost.localstack.cloud:4566"
+    sns            = "http://sns.localhost.localstack.cloud:4566"
+    sqs            = "http://sqs.localhost.localstack.cloud:4566"
   }
 }
 
-resource "aws_s3_bucket" "b3_stock_quotes" {
-  bucket = "b3-stock-quotes"
+# SQS Queue
+
+resource "aws_sqs_queue" "ticker_file_uploaded_queue" {
+  name = "ticker-file-uploaded-queue"
+}
+
+# SNS Topic
+
+resource "aws_sns_topic" "ticker_file_uploaded_notifications" {
+  name = "ticker-file-uploaded-notifications"
+}
+
+resource "aws_sns_topic_subscription" "sqs_subscription" {
+  topic_arn = aws_sns_topic.ticker_file_uploaded_notifications.arn
+  protocol  = "sqs"
+  endpoint  = aws_sqs_queue.ticker_file_uploaded_queue.arn
+}
+
+# S3 Bucket
+
+resource "aws_s3_bucket" "b3_tickers" {
+  bucket = "b3-tickers"
+}
+
+resource "aws_s3_bucket_notification" "ticker_file_uploaded_notifications" {
+  bucket = aws_s3_bucket.b3_tickers.id
+
+  topic {
+    topic_arn = aws_sns_topic.ticker_file_uploaded_notifications.arn
+    events    = ["s3:ObjectCreated:*"]
+  }
+}
+
+# IAM
+
+resource "aws_sns_topic_policy" "ticker_file_uploaded_notifications_policy" {
+  arn = aws_sns_topic.ticker_file_uploaded_notifications.arn
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "s3.amazonaws.com"
+        }
+        Action = "sns:Publish"
+        Resource = aws_sns_topic.ticker_file_uploaded_notifications.arn
+        Condition = {
+          ArnLike = {
+            "aws:SourceArn": aws_s3_bucket.b3_tickers.arn
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_sqs_queue_policy" "ticker_file_uploaded_queue_policy" {
+  queue_url = aws_sqs_queue.ticker_file_uploaded_queue.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "sns.amazonaws.com"
+        }
+        Action = "SQS:SendMessage"
+        Resource = aws_sqs_queue.ticker_file_uploaded_queue.arn
+        Condition = {
+          ArnEquals = {
+            "aws:SourceArn": aws_sns_topic.ticker_file_uploaded_notifications.arn
+          }
+        }
+      }
+    ]
+  })
 }
